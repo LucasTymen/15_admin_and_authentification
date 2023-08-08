faa00d4332d24a90917fab8b39043320c1fe260a
Lucas Tymen <lucas.tymen@gmail.com>
2023-08-08 17:38:12 +0200

    04_User_Objects

--- 04_User_Objects/admin.py
+++ 04_User_Objects/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+# Import Owner and Patient models below:
 from .models import Owner, Patient
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
+# Register your models below:
 admin.site.register(Owner)
 admin.site.register(Patient)
 
 user = User.objects.create_user(username="tom100", email="tom@codecademy.com", password="codec@demy123")
-
 user.save()
