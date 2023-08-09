4135b7731ba3bd4a49efa58d6f7e07822bc85d97
Lucas Tymen <lucas.tymen@gmail.com>
2023-08-09 11:28:44 +0200

    07_Login_Mixin_and_Decorator

--- 07_Login_Mixin_and_Decorator/views.py
+++ 07_Login_Mixin_and_Decorator/views.py
@@ -2,56 +2,57 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 # Import your decorators and mixins below
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Owner, Patient
 from .forms import OwnerCreateForm, OwnerUpdateForm, PatientCreateForm, PatientUpdateForm
 
 # Add the login_required decorator:
-
+@login_required
 def home(request):
    context = {"name": "Django-er"}
    return render(request, "vetoffice/home.html", context)
 
 # Add the LoginRequiredMixin:
-class OwnerList(ListView):
+class OwnerList(LoginRequiredMixin ,ListView):
    model = Owner
 
 # Add the LoginRequiredMixin:
-class PatientList(ListView):
+class PatientList(LoginRequiredMixin, ListView):
     model = Patient
 
 # Add the LoginRequiredMixin:
-class OwnerCreate(CreateView):
+class OwnerCreate(LoginRequiredMixin, CreateView):
    model = Owner
    template_name = "vetoffice/owner_create_form.html"
    form_class = OwnerCreateForm
 
 # Add the LoginRequiredMixin:
-class PatientCreate(CreateView):
+class PatientCreate(LoginRequiredMixin, CreateView):
     model=Patient
     template_name = "vetoffice/patient_create_form.html"
     form_class = PatientCreateForm
 
 # Add the LoginRequiredMixin:
-class OwnerUpdate(UpdateView):
+class OwnerUpdate(LoginRequiredMixin, UpdateView):
    model = Owner
    template_name = "vetoffice/owner_update_form.html"
    form_class = OwnerUpdateForm
 
 # Add the LoginRequiredMixin:
-class PatientUpdate(UpdateView):
+class PatientUpdate(LoginRequiredMixin, UpdateView):
    model = Patient
    template_name = "vetoffice/patient_update_form.html"
    form_class = PatientUpdateForm
 
 # Add the LoginRequiredMixin:
-class OwnerDelete(DeleteView):
+class OwnerDelete(LoginRequiredMixin, DeleteView):
     model = Owner
     template_name = "vetoffice/owner_delete_form.html"
     success_url = "/owner/list"
 
 # Add the LoginRequiredMixin:
-class PatientDelete(DeleteView):
+class PatientDelete(LoginRequiredMixin, DeleteView):
     model = Patient
     template_name = "vetoffice/patient_delete_form.html"
     success_url = "/patient/list"
